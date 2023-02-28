2c2907ddb83118db30d43b83f0cd55929e4da367
takwa202 <takwa.lassoued@esprit.tn>
2023-02-28 18:10:22 +0100

    first commit

---
+++ AM.ApplicationCore/Domain/Passenger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Passenger
+    {
+         public int PassengerId { get; set; }
+        public DateTime BirthDate { get; set; }
+        
+        public int PassportNumber { get; set; }
+
+        public string EmailAddress { get; set;}
+
+        public string FirstName { get; set;}
+        
+        public string LastName { get; set;}
+
+        public string TelNumber { get; set;}
+        public ICollection<Flight> Flights { get; set; }
+        public Passenger() { }
+        public Passenger(DateTime birthDate, int passportNumber, string emailAddress, string firstName, string lastName, string telNumber, ICollection<Flight> flights)
+        {
+            BirthDate = birthDate;
+            PassportNumber = passportNumber;
+            EmailAddress = emailAddress;
+            FirstName = firstName;
+            LastName = lastName;
+            TelNumber = telNumber;
+            Flights = flights;
+        }
+        public Passenger(DateTime birthDate, int passportNumber, string emailAddress, string firstName, string lastName, string telNumber)
+        {
+            BirthDate = birthDate;
+            PassportNumber = passportNumber;
+            EmailAddress = emailAddress;
+            FirstName = firstName;
+            LastName = lastName;
+            TelNumber = telNumber;
+            
+        }
+
+
+        public bool checkProfile (string firstName,string lastName)
+        {
+            return FirstName == firstName && LastName == lastName;
+        }
+        public bool checkProfile(string firstName, string lastName,string mail)
+        {
+            return FirstName == firstName && LastName == lastName && mail == EmailAddress;
+        }
+
+        public virtual void PassengerType()
+        {
+            Console.WriteLine("I am a Passenger");
+        }
+
+    }
+}
---
+++ AM.ApplicationCore/Domain/Plane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public enum PlaneType { Boing,Airbus}
+    public class Plane
+    {
+        
+        public int Capacity { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public int PlaneId { get; set; }
+        public string AirlineLogo { get; set; }
+        public PlaneType PlaneType { get; set; }
+        public ICollection<Flight> Flights { get; set; }
+      
+    }
+}
